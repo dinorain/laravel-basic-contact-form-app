8707ac075326449cfb8cea2b76880d10dc2ab5f0
Dustin <djourdan555@gmail.com>
2021-02-05 10:43:51 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactMessageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group([
+    'prefix' => 'contact-messages'
+], function () {
+    Route::get('/', [ContactMessageController::class, 'index'])->name('contact_message.index');
+    Route::get('/create', [ContactMessageController::class, 'create'])->name('contact_message.create');
+    Route::get('/{id}', [ContactMessageController::class, 'show'])->name('contact_message.show');
+    Route::get('/{id}/edit', [ContactMessageController::class, 'edit'])->name('contact_message.edit');
+
+    Route::post('/', [ContactMessageController::class, 'store'])->name('contact_message.store');
+    Route::post('/{id}', [ContactMessageController::class, 'update'])->name('contact_message.update');
+    Route::delete('/{id}', [ContactMessageController::class, 'destroy'])->name('contact_message.destroy');
 });
